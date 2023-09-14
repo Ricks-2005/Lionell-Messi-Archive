a26737f9fb4dfd9d67990bd26651ef731cdc213f
Priyansh Bharti <pokemonrick1234@gmail.com>
2023-09-15 02:45:09 +0530

    Update mongo.py

--- Messi/mongo.py
+++ Messi/mongo.py
@@ -1,26 +1,17 @@
 import asyncio
 import sys
-import logging as log
-
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-
-MONGO_DB_URI = "mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority"
 from Messi.confing import get_int_key, get_str_key
+from Messi import LOGGER
 
-
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Messi"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-db = client["spamguardianbot"]
+client = MongoClient("mongodb+srv://enmu:enmu123@enmu.2cuev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://enmu:enmu123@enmu.2cuev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 27017)["Messi"]
+motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://enmu:enmu123@enmu.2cuev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 27017)
+db = motor["Messi"]
+db = client["Messi"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(LOGGER.critical("ERROR | DB CONNECTION BREACHED"))
