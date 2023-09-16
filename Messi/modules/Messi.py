86f2555adbb75a84fe2d8181aa7e950c35ed1a48
Priyansh Bharti <pokemonrick1234@gmail.com>
2023-09-16 06:38:53 +0530

    Update Messi.py

--- Messi/modules/Messi.py
+++ Messi/modules/Messi.py
@@ -1,4 +1,4 @@
-'''
+
 import os
 import re
 import random
@@ -13,11 +13,11 @@ PHOTO = "https://te.legra.ph/file/dcb028440757ea5153898.jpg"
 
 @register(pattern=("activate"))
 async def awake(event):
-  TEXT += f"Hi {event.sender.first_name}, I'm SpamGuardian."\n\n
+  TEXT += f"Hi {event.sender.first_name}, I'm **SpamGuardian**.\n\n"
   TEXT += f"❄ SpamGuardian Is Alive **"\n\n
-  TEXT += f"❄ My Domain : [Primes](https://t.me/PrimesDivision)**"\n\n
-  TEXT += f"❄**Powered By: [Primes Team](https://t.me/PrimesxTech)**"\n\n
+  TEXT += f"❄ My Domain : [Primes](https://t.me/PrimesDivision)**\n\n"
+  TEXT += f"❄**Powered By: [Primes Team](https://t.me/PrimesxTech)**\n\n"
    Thanks For Adding Me Here ❤️ re ❤️ **"
   BUTTON = [[Button.url("Help", "https://t.me/spamguardianbot?start=help"), Button.url("My Home", "https://t.me/TeleSupportChat")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
-'''
+
